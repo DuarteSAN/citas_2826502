3ad233d8b94e613638c0b052f62b28533f2f9f7f
unknown <Administrador@BOGDFPCGMF4137.FormacionSena.RED>
2024-02-27 16:49:46 -0500

    Entidades y relaciones terminadas

--- app/models.py
+++ app/models.py
@@ -2,13 +2,38 @@
 #Definir los atributos de objeto
 #Pero con tipos traducibles a SQL y Mysql
 from app import db
+from datetime import datetime
 
-class medico(db.Model):
+class Medico(db.Model):
+    __tablename__ = "medico"
     id = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(120), nullable = True)
     apellido = db.Column(db.String(120), nullable = True)
     tipo_identificacion = db.Column(db.String(4), nullable = True)
     numero_identificacion = db.Column(db.Integer)
-    registro_medico = db.Column(db.String)
+    registro_medico = db.Column(db.String(120))
     especialidad = db.Column(db.String(120))
+
+class Paciente(db.Model):
+    __tablename__ = "pacientes"
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(120), nullable = True)
+    apellido = db.Column(db.String(120), nullable = True)
+    tipo_identificacion = db.Column(db.String(4), nullable = True)
+    numero_identificacion = db.Column(db.Integer)
+    altura = db.Column(db.Integer)
+    tipo_sangre = db.Column(db.String(2))
+
+class Consultorio(db.Model):
+    __tablename__ = "consultorio"
+    id = db.Column(db.Integer, primary_key = True)
+    numero = db.Column(db.Integer)
+
+class Cita(db.Model):
+    __tablename__ = "citas"
+    id = db.Column(db.Integer, primary_key = True)
+    fecha = db.Column(db.DateTime, default = datetime.utcnow)
+    paciente_id = db.Column(db.Integer , db.ForeignKey("pacientes.id"))
+    medico_id = db.Column(db.Integer , db.ForeignKey("medico.id"))
+    consultorio_id = db.Column(db.Integer , db.ForeignKey("consultorio.id"))
     
